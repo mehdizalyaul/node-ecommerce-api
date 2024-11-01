47c501d9bd2ac05af1f77b753af78095b5bbeec6
Mehdi ZALYAUL <mehdi.zalyaul@gmail.com>
2024-11-01 20:16:21 +0100

    refactor: add seeding in more realistic way

--- backend/seeders/20241101135458-users.js
+++ backend/seeders/20241101135458-users.js
@@ -12,62 +12,106 @@ const {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // Create users
-    const users = Array.from({ length: 10 }).map(() => ({
-      name: faker.internet.username(),
-      password: faker.internet.password(),
-      email: faker.internet.email(),
-    }));
-    const createdUsers = await User.bulkCreate(users);
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      // Create users
+      const users = Array.from({ length: 10 }).map(() => ({
+        name: faker.person.firstName(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+      }));
+      const createdUsers = await User.bulkCreate(users, { transaction });
 
-    // Create categories
-    const categories = Array.from({ length: 8 }).map(() => ({
-      name: faker.commerce.department(),
-    }));
-    const createdCategories = await Category.bulkCreate(categories);
+      // Create categories
+      const categories = Array.from({ length: 8 }).map(() => ({
+        name: faker.commerce.department(),
+      }));
+      const createdCategories = await Category.bulkCreate(categories, {
+        transaction,
+      });
 
-    // Create products and associate with categories
-    const products = Array.from({ length: 20 }).map(() => ({
-      images: Array.from({ length: 3 }).map(() => faker.image.avatar()),
-      name: faker.commerce.productName(),
-      description: faker.commerce.productDescription(),
-      price: parseFloat(faker.commerce.price({ min: 100, max: 500 })),
-      stock: faker.number.int({ max: 100 }),
-      categoryId: faker.helpers.arrayElement(createdCategories).id,
-    }));
-    const createdProducts = await Product.bulkCreate(products);
+      // Create products
+      const products = Array.from({ length: 20 }).map(() => ({
+        images: Array.from({ length: 3 }).map(() => faker.image.avatar()),
+        name: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        price: parseFloat(faker.commerce.price({ min: 100, max: 500 })),
+        stock: faker.number.int({ max: 100 }),
+        categoryId: faker.helpers.arrayElement(createdCategories).id,
+      }));
+      const createdProducts = await Product.bulkCreate(products, {
+        transaction,
+      });
 
-    // Create cart items
-    const cartItems = Array.from({ length: 12 }).map(() => ({
-      userId: faker.helpers.arrayElement(createdUsers).id,
-      productId: faker.helpers.arrayElement(createdProducts).id,
-      quantity: faker.number.int({ max: 50 }),
-    }));
-
-    const createdCartItems = await CartItem.bulkCreate(cartItems);
-
-    // Create orders associated with users
-    const orders = Array.from({ length: 10 }).map(() => {
-      return {
+      // Create cart items
+      const cartItems = Array.from({ length: 12 }).map(() => ({
         userId: faker.helpers.arrayElement(createdUsers).id,
-        address: faker.location.street(),
-        paymentMethod: faker.finance.transactionType(),
-      };
-    });
-    const createdOrders = await Order.bulkCreate(orders);
+        productId: faker.helpers.arrayElement(createdProducts).id,
+        quantity: faker.number.int({ max: 50 }),
+      }));
+      const createdCartItems = await CartItem.bulkCreate(cartItems, {
+        transaction,
+        ignoreDuplicates: true,
+      });
 
-    // Create order items associated with orders and products
-    const orderItems = Array.from({ length: 25 }).map(() => {
-      const selectedProduct = faker.helpers.arrayElement(createdProducts);
-      const quantity = faker.number.int({ max: 5, min: 1 });
-      return {
-        orderId: faker.helpers.arrayElement(createdOrders).id,
-        productId: selectedProduct.id,
-        quantity,
-        price: selectedProduct.price * quantity,
-      };
-    });
-    await OrderItem.bulkCreate(orderItems);
+      // Fetch products beforehand
+      const productsList = await Product.findAll();
+      const productsMap = productsList.reduce((map, product) => {
+        map[product.id] = product;
+        return map;
+      }, {});
+
+      for (const user of createdUsers) {
+        const userCarts = createdCartItems.filter(
+          (cart) => cart.userId === user.id
+        );
+        if (userCarts.length === 0) continue;
+
+        let userTotalPrice = 0;
+        const orderItems = userCarts
+          .map((userCart) => {
+            const product = productsMap[userCart.productId];
+            if (product) {
+              const totalProductPrice = product.price * userCart.quantity;
+              userTotalPrice += totalProductPrice;
+              return {
+                productId: product.id,
+                quantity: userCart.quantity,
+                totalPrice: totalProductPrice,
+              };
+            }
+          })
+          .filter((item) => item !== undefined);
+
+        const createdOrder = await Order.create(
+          {
+            userId: user.id,
+            totalAmount: userTotalPrice,
+            address: faker.location.street(),
+            paymentMethod: faker.finance.transactionType(),
+          },
+          { transaction }
+        );
+        await Promise.all(
+          orderItems.map((item) =>
+            OrderItem.create(
+              {
+                orderId: createdOrder.id,
+                productId: item.productId,
+                quantity: item.quantity,
+                totalPrice: item.totalPrice,
+              },
+              { transaction }
+            )
+          )
+        );
+      }
+
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      console.error("Transaction rolled back due to error:", error);
+    }
   },
 
   async down(queryInterface, Sequelize) {
