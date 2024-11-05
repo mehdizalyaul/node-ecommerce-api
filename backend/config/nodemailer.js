12b2270d90a43f8f451f7a1960dd2fb7fea4d847
Mehdi ZALYAUL <mehdi.zalyaul@gmail.com>
2024-11-05 15:35:38 +0100

    refactor: move nodemailer configuration to config folder

---
+++ backend/config/nodemailer.js
@@ -0,0 +1,12 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.TRANSPORT_HOST,
+  port: process.env.TRANSPORT_PORT,
+  auth: {
+    user: process.env.TRANSPORT_USER,
+    pass: process.env.TRANSPORT_PASSWORD,
+  },
+});
+
+module.exports = transporter;
