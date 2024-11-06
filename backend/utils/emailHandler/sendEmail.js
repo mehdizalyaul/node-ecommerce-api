6728925b8f6e3ef5c9f3067b5622a15a8f5ca2b2
Mehdi ZALYAUL <mehdi.zalyaul@gmail.com>
2024-11-06 20:34:07 +0100

    refactor: centralize email sending into reusable sendEmail function

--- backend/utils/emailHandler/sendEmail.js
+++ backend/utils/emailHandler/sendEmail.js
@@ -1,44 +1,10 @@
 const transporter = require("../../config/nodemailer.js");
-const { verifyEmailInfo, resetEmailInfo } = require("./emailResponse.js");
-const generateToken = require("./tokenUtils.js");
 
-async function sendEmail(user, path) {
-  let subject;
-  let html;
-
-  if (path === "/register" || "/resend-verify-email") {
-    subject = "User verification email";
-
-    // Generate verification token for user registration
-    const { token: verifyEmailToken, tokenHash: verifyEmailTokenHashed } =
-      await generateToken();
-
-    user.verifyEmailToken = verifyEmailTokenHashed;
-    user.verifyEmailTokenExpire =
-      Date.now() + parseInt(process.env.TOKEN_EXPIRATION_TIME);
-    await user.save();
-
-    html = await verifyEmailInfo(verifyEmailToken, user);
-  } else if (path === "/forgetpassword") {
-    subject = "Password reset email";
-
-    // Generate reset token for password reset
-    const { token: resetToken, tokenHash: resetTokenHash } =
-      await generateToken();
-
-    user.resetToken = resetTokenHash;
-    user.resetTokenExpire =
-      Date.now() + parseInt(process.env.TOKEN_EXPIRATION_TIME);
-    await user.save();
-
-    html = await resetEmailInfo(resetToken, user);
-  }
-
-  // Send the email
-  await transporter.sendMail({
-    to: user.email,
-    subject: subject,
-    html: html,
+async function sendEmail(to, subject, content) {
+  return await transporter.sendMail({
+    to,
+    subject,
+    html: content,
   });
 }
 
